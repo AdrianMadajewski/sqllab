e1212adf305d15e7147eab1f1a30d5ee978395c6
Adrian Madajewski <madajewski.adrian@gmail.com>
2021-11-08 21:32:30 +0100

    lab3 added

--- lab3.sql
+++ lab3.sql
@@ -98,3 +98,246 @@ BEGIN
 END PokazPracownikowEtatu;
 
 EXEC PokazPracownikowEtatu('PROFESOR');
+
+-- 6.
+CREATE OR REPLACE PROCEDURE RaportKadrowy IS
+  CURSOR cEtaty IS
+  SELECT nazwa
+  FROM etaty
+  ORDER BY nazwa;
+
+  CURSOR cPracownicy(
+    pEtat pracownicy.etat%type
+  ) IS
+  SELECT nazwisko, (placa_pod + COALESCE(placa_dod, 0)) AS pensja
+  FROM pracownicy
+  WHERE etat = pEtat;
+
+  vSumaPensji NUMBER := 0;
+  vLicznik NUMBER := 0;
+BEGIN
+  FOR vEtat IN cEtaty LOOP
+    vSumaPensji := 0;
+    vLicznik := 0;
+    DBMS_OUTPUT.PUT_LINE('Etat: ' || vEtat.nazwa);
+    DBMS_OUTPUT.PUT_LINE('------------------------------');
+    FOR vPracownik IN cPracownicy(vEtat.nazwa) LOOP
+      vLicznik := vLicznik + 1;
+      vSumaPensji := vSumaPensji + vPracownik.pensja;
+      DBMS_OUTPUT.PUT_LINE(cPracownicy%ROWCOUNT || '. ' || vPracownik.nazwisko || ', pensja: ' || vPracownik.pensja);
+    END LOOP;
+
+    DBMS_OUTPUT.PUT_LINE('Liczba pracownikow: ' || vLicznik);
+
+     IF vLicznik = 0 THEN
+      DBMS_OUTPUT.PUT_LINE('Średnia pensja: brak');
+    ELSE
+      DBMS_OUTPUT.PUT_LINE('Średnia pensja: ' || (vSumaPensji / vLicznik));
+    END IF;
+    DBMS_OUTPUT.PUT_LINE('');
+  END LOOP;
+
+END;
+
+EXEC RaportKadrowy;
+
+-- 7.
+CREATE OR REPLACE PACKAGE IntZespoly IS
+    PROCEDURE DodajZespol(
+        pNazwa zespoly.nazwa%type,
+        pAdres zespoly.adres%type
+    );
+    PROCEDURE UsunZespolId(
+        pIdZesp zespoly.id_zesp%type
+    );
+    PROCEDURE UsunZespolNazwa(
+        pNazwa zespoly.nazwa%type
+    );
+    PROCEDURE ModyfikujZespol(
+        pIdZesp zespoly.id_zesp%type,
+        pNazwa zespoly.nazwa%type,
+        pAdres zespoly.adres%type
+    );
+    FUNCTION ZnajdzIdZespolu( pNazwa zespoly.nazwa%type ) RETURN zespoly.id_zesp%type;
+    FUNCTION ZnajdzNazweZespolu( pIdZesp zespoly.id_zesp%type ) RETURN zespoly.nazwa%type;
+    FUNCTION ZnajdzAdresZespolu( pIdZesp zespoly.id_zesp%type ) RETURN zespoly.adres%type;
+END IntZespoly;
+
+CREATE OR REPLACE PACKAGE BODY IntZespoly IS
+    PROCEDURE DodajZespol(
+        pNazwa zespoly.nazwa%type,
+        pAdres zespoly.adres%type
+    ) IS
+    BEGIN
+        INSERT INTO zespoly (id_zesp, nazwa, adres)
+        VALUES ((SELECT MAX(id_zesp) + 1 FROM zespoly), pNazwa, pAdres);
+
+        IF SQL%NOTFOUND THEN
+          DBMS_OUTPUT.PUT_LINE('Nie udało się dodac zespolu o nazwie: ' || pNazwa);
+        END IF;
+    END;
+
+    PROCEDURE UsunZespolId(
+        pIdZesp zespoly.id_zesp%type
+    ) IS
+    BEGIN
+        DELETE FROM zespoly
+        WHERE id_zesp = pIdZesp;
+
+        IF SQL%NOTFOUND THEN
+          DBMS_OUTPUT.PUT_LINE('Nie udało się usunac zespolu od id: ' || pIdZesp);
+        END IF;
+    END;
+
+    PROCEDURE UsunZespolNazwa(
+        pNazwa zespoly.nazwa%type
+    ) IS
+    BEGIN
+        DELETE FROM zespoly
+        WHERE nazwa = pNazwa;
+
+        IF SQL%NOTFOUND THEN
+          DBMS_OUTPUT.PUT_LINE('Nie udało się usunac zespolu o nazwie: ' || pNazwa);
+        END IF;
+    END;
+
+    PROCEDURE ModyfikujZespol(
+        pIdZesp zespoly.id_zesp%type,
+        pNazwa zespoly.nazwa%type,
+        pAdres zespoly.adres%type
+    ) IS
+    BEGIN
+        UPDATE zespoly
+        SET
+            nazwa = pNazwa,
+            adres = pAdres
+        WHERE id_zesp = pIdZesp;
+
+        IF SQL%NOTFOUND THEN
+          DBMS_OUTPUT.PUT_LINE('Nie udało się zmodyfikowac danych zespolu o id: ' || pIdZesp);
+        END IF;
+    END;
+
+    FUNCTION ZnajdzIdZespolu(
+        pNazwa zespoly.nazwa%type
+    ) RETURN zespoly.id_zesp%type IS
+        vIdZesp zespoly.id_zesp%type;
+    BEGIN
+        SELECT ID_ZESP INTO vIdZesp FROM zespoly WHERE nazwa = pNazwa;
+        RETURN vIdZesp;
+    END;
+
+    FUNCTION ZnajdzNazweZespolu(
+        pIdZesp zespoly.id_zesp%type
+    ) RETURN zespoly.nazwa%type IS
+        vNazwa zespoly.nazwa%type;
+    BEGIN
+        SELECT nazwa INTO vNazwa FROM zespoly WHERE id_zesp = pIdZesp;
+        RETURN vNazwa; 
+    END;
+
+    FUNCTION ZnajdzAdresZespolu(
+        pIdZesp zespoly.id_zesp%type
+    ) RETURN zespoly.adres%type IS
+        vAdres zespoly.adres%type;
+    BEGIN
+        SELECT adres INTO vAdres FROM zespoly WHERE id_zesp = pIdZesp;
+        RETURN vAdres;
+    END;
+END IntZespoly;
+
+EXEC IntZespoly.DODAJZESPOL('Zespoly NOWY', 'Adres nowy');
+
+SELECT * FROM zespoly;
+
+-- 8.
+CREATE OR REPLACE PACKAGE BODY IntZespoly IS
+    PROCEDURE DodajZespol(
+        pNazwa zespoly.nazwa%type,
+        pAdres zespoly.adres%type
+    ) IS
+    exIstniejeNazwa EXCEPTION;
+    exIstniejeID EXCEPTION;
+    BEGIN
+        DECLARE
+            CURSOR cID IS
+                SELECT id_zesp FROM zespoly ORDER BY id_zesp ASC;
+            CURSOR cNazwa IS
+                SELECT nazwa FROM zespoly ORDER BY nazwa ASC;
+
+        INSERT INTO zespoly (id_zesp, nazwa, adres)
+        VALUES ((SELECT MAX(id_zesp) + 1 FROM zespoly), pNazwa, pAdres);
+
+        IF SQL%NOTFOUND THEN
+          DBMS_OUTPUT.PUT_LINE('Nie udało się dodac zespolu o nazwie: ' || pNazwa);
+        END IF;
+    END;
+
+    PROCEDURE UsunZespolId(
+        pIdZesp zespoly.id_zesp%type
+    ) IS
+    BEGIN
+        DELETE FROM zespoly
+        WHERE id_zesp = pIdZesp;
+
+        IF SQL%NOTFOUND THEN
+          DBMS_OUTPUT.PUT_LINE('Nie udało się usunac zespolu od id: ' || pIdZesp);
+        END IF;
+    END;
+
+    PROCEDURE UsunZespolNazwa(
+        pNazwa zespoly.nazwa%type
+    ) IS
+    BEGIN
+        DELETE FROM zespoly
+        WHERE nazwa = pNazwa;
+
+        IF SQL%NOTFOUND THEN
+          DBMS_OUTPUT.PUT_LINE('Nie udało się usunac zespolu o nazwie: ' || pNazwa);
+        END IF;
+    END;
+
+    PROCEDURE ModyfikujZespol(
+        pIdZesp zespoly.id_zesp%type,
+        pNazwa zespoly.nazwa%type,
+        pAdres zespoly.adres%type
+    ) IS
+    BEGIN
+        UPDATE zespoly
+        SET
+            nazwa = pNazwa,
+            adres = pAdres
+        WHERE id_zesp = pIdZesp;
+
+        IF SQL%NOTFOUND THEN
+          DBMS_OUTPUT.PUT_LINE('Nie udało się zmodyfikowac danych zespolu o id: ' || pIdZesp);
+        END IF;
+    END;
+
+    FUNCTION ZnajdzIdZespolu(
+        pNazwa zespoly.nazwa%type
+    ) RETURN zespoly.id_zesp%type IS
+        vIdZesp zespoly.id_zesp%type;
+    BEGIN
+        SELECT ID_ZESP INTO vIdZesp FROM zespoly WHERE nazwa = pNazwa;
+        RETURN vIdZesp;
+    END;
+
+    FUNCTION ZnajdzNazweZespolu(
+        pIdZesp zespoly.id_zesp%type
+    ) RETURN zespoly.nazwa%type IS
+        vNazwa zespoly.nazwa%type;
+    BEGIN
+        SELECT nazwa INTO vNazwa FROM zespoly WHERE id_zesp = pIdZesp;
+        RETURN vNazwa; 
+    END;
+
+    FUNCTION ZnajdzAdresZespolu(
+        pIdZesp zespoly.id_zesp%type
+    ) RETURN zespoly.adres%type IS
+        vAdres zespoly.adres%type;
+    BEGIN
+        SELECT adres INTO vAdres FROM zespoly WHERE id_zesp = pIdZesp;
+        RETURN vAdres;
+    END;
+END IntZespoly;
